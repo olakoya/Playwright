79983c1295113cf62474b3dafe19c429cb036ecd
olakoya <ola.koya1@gmail.com>
2025-08-04 23:32:00 +0100

    updates

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -13,6 +13,38 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
 });
+
+
+/**
+ * 1. Open the page => Nos 37
+ * 2. Click at Get started => Nos 39
+ * 3. Mouse hover the language dropdown => Nos 40
+ * 4. Click at Java => Nos 41
+ * 5. Check the URL => Nos 44
+ * 6. Check the text "Installing Playwright" is not being displayed => Nos 45
+ * 7. Check the text below is displayed => Nos 47 - 48
+ * 
+ * Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.
+ * 
+ */
+
+// test.only('check Java page', async ({ page }) => {
+test.only('check Java page', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('button', { name: 'Node.js' }).hover();
+  await page.getByText('Java', { exact: true }).click();
+
+  // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
+  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+  await expect(page.getByText('Installing Playwright', { exact: true })).not.toBeVisible();
+
+  const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+  await expect(page.getByText(javaDescription)).toBeVisible();
+});
+
